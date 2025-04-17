65c9a0de1afdf75692c23a390b86b02327908ccf
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-17 19:22:20 -0300

    feat: stripe checkout session subscriotion

---
+++ src/app/api/stripe/create-subscription-checkout/route.ts
@@ -0,0 +1,35 @@
+import stripe from "@/app/lib/stripe";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest){
+    const { testId } = await req.json();
+
+    const price = process.env.STRIPE_SUBSCRIPTION_PRICE_ID;
+
+    if(!price) 
+        return NextResponse.json({error: 'Price not found'}, { status: 500 });
+
+    const metadata = {
+        testId
+    };
+
+    try {
+        const session = await stripe.checkout.sessions.create({
+            line_items: [{ price, quantity: 1}],
+            mode: 'subscription',	
+            payment_method_types: ['card'],
+            success_url: `${req.headers.get('origin')}/success`,
+            cancel_url: `${req.headers.get('origin')}/`,
+            metadata
+        });
+
+        if(!session.url) 
+            return NextResponse.json({error: 'Session URL not found'}, { status: 500 });
+
+        return NextResponse.json({ sessionId: session.id }, { status: 200 });
+    } catch (error) {
+        console.error('Error creating checkout session:', error);
+
+        return NextResponse.error();
+    }
+}
