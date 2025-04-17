52e6a58aa27b40a777d4269e701c9f57d2efaa8c
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2025-04-16 21:28:04 -0300

    feat: stripe lib

---
+++ src/app/lib/stripe.ts
@@ -0,0 +1,10 @@
+import Stripe from 'stripe';
+
+if(!process.env.STRIPE_SECRET_KEY)
+    throw new Error('Missing Stripe secret key in environment variables');
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+    apiVersion: '2025-03-31.basil',
+});
+
+export default stripe;
